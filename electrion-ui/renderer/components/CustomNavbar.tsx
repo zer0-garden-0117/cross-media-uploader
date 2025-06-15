ec0d2639f1511a3be582a87bc8198ecfee5e03fd
zer0 <zer0.garden.0117@gmail.com>
2025-06-15 16:08:43 +0900

    add Mantine UI AppShellLayout, CustomNavbar

---
+++ electrion-ui/renderer/components/CustomNavbar.tsx
@@ -0,0 +1,36 @@
+import {
+  IconHome2,
+  IconSettings
+} from '@tabler/icons-react';
+import { Stack, Tooltip, UnstyledButton } from '@mantine/core';
+import Link from 'next/link'
+
+interface NavbarLinkProps {
+  icon: typeof IconHome2;
+  label: string;
+  link?: string;
+}
+
+function NavbarLink({ icon: Icon, label, link }: NavbarLinkProps) {
+  return (
+    <Link href={link}>
+      <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+        <UnstyledButton>
+          <Icon size={20} stroke={1.5} />
+        </UnstyledButton>
+      </Tooltip>
+    </Link>
+  );
+}
+
+export function CustomNavbar() {
+  return (
+    <nav>
+      <Stack justify="center" gap={10}>
+        <NavbarLink icon={IconHome2} label="Home" link='/home'/>
+        <NavbarLink icon={IconSettings} label="Post" link='/post'/>
+        <NavbarLink icon={IconSettings} label="Edit" link='/edit'/>
+      </Stack>
+    </nav>
+  );
+}
--- electrion-ui/renderer/pages/_app.tsx
+++ electrion-ui/renderer/pages/_app.tsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
 
 import '../styles/globals.css'
 import '@mantine/core/styles.css';
 import '@mantine/dropzone/styles.css';
 import '@mantine/dates/styles.css';
 import { createTheme, MantineProvider } from '@mantine/core';
+import { CustomNavbar } from '../components/CustomNavbar';
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
+function AppShellLayout({ children }: { children: React.ReactNode }) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      navbar={{ width: 50, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        <CustomNavbar />
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
-      <Component {...pageProps} />
+      <AppShellLayout>
+        <Component {...pageProps} />
+      </AppShellLayout>
     </MantineProvider>
   )
 }
